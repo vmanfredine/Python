eb21935f877b2ad120260e02e2fbc7a5afda7072
Vitor Manfredine <69250433+vmanfredine@users.noreply.github.com>
2026-04-08 11:06:40 -0300

    biblioteca rich

---
+++ POO/rich/rich001.py
@@ -0,0 +1,7 @@
+from rich import print
+
+print("Olá, [red]Mundo[/]! :earth_americas:")
+print("Olá, [bold blue on white]Nerd[/] :vulcan_salute:")
+print("Vamos pra cima! :airplane_departure:")
+
+# Emojis - terminal > python -m rich.emoji
---
+++ POO/rich/rich002.py
@@ -0,0 +1,5 @@
+from rich import print
+from rich.panel import Panel
+
+caixa = Panel("[white]Esse aqui é um painel de exemplo[/]:+1:", title="Caixa", style="red", width=40)
+print(caixa)
